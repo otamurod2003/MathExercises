c79c991cd91481db3cc87263db88376d09f3b706
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-16 17:10:03 +0500

    Added project PrimeNumber

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿//Kiritilgan son tub yoki tub emasligini aniqlovchi dastur
+
+int number;
+
+Console.Write("Son kiriting: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+var yaxlit = Math.Ceiling(Math.Sqrt(number));
+if(number == 0 || number==1)
+    Console.WriteLine("Tub son emas");
+else
+{
+    for (int i = 2; i < yaxlit; i++)
+    {
+        if(yaxlit % i == 0)
+        {
+            Console.WriteLine("Tub son emas");
+        }
+        else
+        {
+            Console.WriteLine($"{i} Tub son");
+        }
+    }
+}
