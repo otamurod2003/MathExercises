f9bd4aef842a9966a89c24aa05692f5a6889a774
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-16 16:28:34 +0500

    Created new repo

---
+++ CountSpacesString/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Write("Matn kiriting: ");
+ string words = Console.ReadLine();
+
+ int space = 0;
+       for (int i = 0; i < words.Length; i++)
+       {
+            if (words[i] == ' ')
+            {
+             space++;
+            }
+       }
+Console.WriteLine("Spaces: " + space);
+Console.ReadKey();          
