4dac3861597cda560f9ca569420415471a17682d
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-21 12:30:22 +0500

    Added new project name is "FindNumberInString"

---
+++ FindNumberInString/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Satr kiriting: ");
+string satr = Console.ReadLine();
+string numbers = "0123456789";
+int sumNumInString = 0;
+try
+{
+    for (int i = 0; i < satr.Length; i++)
+    {
+        for(int k=0; k<numbers.Length; k++)
+        {
+            if (numbers[k] == satr[i])
+            {
+                Console.Write(satr[i]+ " ");
+                sumNumInString += satr[i];
+            }
+        }
+    }
+    Console.WriteLine(sumNumInString);
+    
+    
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
