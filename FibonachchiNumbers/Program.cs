5a2437d762573cfed11c026c010f7aed7d50c359
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-03-02 22:20:40 +0500

    Added fibonachchi project

---
+++ FibonachchiNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers=null;
+        int[] fibonaci=null;
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        for(int i=2; i<10; i++)
+        {
+            numbers[i] = numbers[i] + numbers[i-1];
+            fibonaci[i]= numbers[i];
+        }
+
+        Console.WriteLine(fibonaci);
+    }
+}
