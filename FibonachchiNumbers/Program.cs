2ee1cf6c451e76fcb5b4053b9ff3fc72efef2809
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-03-04 09:52:22 +0500

    fibonacci numbers code changed

--- FibonachchiNumbers/Program.cs
+++ FibonachchiNumbers/Program.cs
@@ -1,18 +1,27 @@
-﻿class Program
+﻿/*static void Fibonacci_Iterative(int len)
 {
-    static void Main(string[] args)
+   int a = 0, b = 1, c = 0;
+   Console.Write("{0} {1}", a, b);
+   for (int i = 2; i < len; i++)
+   {
+       c = a + b;
+       Console.Write(" {0}", c);
+       a = b;
+       b = c;
+   }
+}
+
+Fibonacci_Iterative(20);*/
+
+
+   
+
+ static void Fibonacci_Rec_Temp(int a, int b, int counter, int len)
+{
+    if (counter <= len)
     {
-        int[] numbers=null;
-        int[] fibonaci=null;
-        numbers[0] = 1;
-        numbers[1] = 2;
-
-        for(int i=2; i<10; i++)
-        {
-            numbers[i] = numbers[i] + numbers[i-1];
-            fibonaci[i]= numbers[i];
-        }
-
-        Console.WriteLine(fibonaci);
+        Console.Write("{0} ", a);
+        Fibonacci_Rec_Temp(b, a + b, counter + 1, len);
     }
 }
+Fibonacci_Rec_Temp(0, 1, 1, 15);
