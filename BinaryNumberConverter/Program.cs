d157725d20b41f0703c96d699eee3e2dea4a4be5
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-03-31 15:48:56 +0500

    Added new Math proj in the solution

---
+++ BinaryNumberConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿// decimal to binary
+Console.WriteLine("O'nlik sanoq sistemasidagi sonni kiriting");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+string binaryNumber = Convert.ToString(decimalNumber, 2);
+
+Console.WriteLine(binaryNumber);
+
+//binary to decimal
+Console.Write("Ikkilik sanoq sistemasidagi sonni kiriting: ");
+string binary = Console.ReadLine();
+
+int decimalNum = Convert.ToInt32(binary, 2);
+Console.WriteLine("Number: " + decimalNum);
---
+++ MathEx/Program.cs
@@ -0,0 +1,5 @@
+﻿double e = 2.7182818284590452353602874713527;
+double x = 2.444, y = 0.869 * Math.Pow(10, -2), z =-0.13*Math.Pow(10, 3);
+var h = ((Math.Pow(x, y - 1) + Math.Pow(e, y - 1)
+    / (1 + x * (Math.Abs(y - Math.Tan(z)) * (1 + Math.Abs(y - x)) + (Math.Pow(Math.Abs(x - y), 2) / 2) - (Math.Pow(Math.Abs(y - x), 2) / 3)))));
+Console.WriteLine(h);
