76405aedd4a66f0ffe5f8b4a8ce6939968610072
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-05-27 09:25:06 +0500

    Added new FizzBuzz algorithm

--- BinaryNumberConverter/Program.cs
+++ BinaryNumberConverter/Program.cs
@@ -1,14 +1,45 @@
-﻿// decimal to binary
-Console.WriteLine("O'nlik sanoq sistemasidagi sonni kiriting");
-int decimalNumber = Convert.ToInt32(Console.ReadLine());
+﻿//Selecting numeration system
+Console.Write("Qaysi sanoq tizimidan o'tkazamiz: ");
+int fromSelectNumerationSystem = Convert.ToInt32(Console.ReadLine());
+Console.Write("Qaysi sanoq tizimiga o'tkazamiz: ");
+int doSelectNumerationSystem = Convert.ToInt32(Console.ReadLine());
+if (fromSelectNumerationSystem < 2 || doSelectNumerationSystem > 16)
+{
+    Console.WriteLine("Notog'ri sanoq tizimlari ko'rsatildi");
+}
+else
+{
+    Console.WriteLine($"{fromSelectNumerationSystem} sanoq tizimidagi sonni kiriting: ");
+    int firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{doSelectNumerationSystem} sanoq tizimidagi sonni kiriting: ");
+    int secondNumber = int.Parse(Console.ReadLine());
+    var firstNumToStr = firstNumber.ToString();
+    var secondNumToStr = secondNumber.ToString();
+    foreach (int firstNumberElement in firstNumToStr)
+    {
+        if (firstNumberElement >= fromSelectNumerationSystem) { Console.WriteLine(firstNumberElement + "is not defined ?");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Okay");
+        }
+    }
+    foreach (int secondNumberElement in firstNumToStr)
+    {
+        if (secondNumberElement >= doSelectNumerationSystem)
+        {
+            Console.WriteLine(secondNumberElement + "is not defined ?");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Okay");
+        }
+    }
 
-string binaryNumber = Convert.ToString(decimalNumber, 2);
+}
 
-Console.WriteLine(binaryNumber);
 
-//binary to decimal
-Console.Write("Ikkilik sanoq sistemasidagi sonni kiriting: ");
-string binary = Console.ReadLine();
 
-int decimalNum = Convert.ToInt32(binary, 2);
-Console.WriteLine("Number: " + decimalNum);
+
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter number : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+if(a % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+if ((a % 5 == 0)&&(a % 3 == 0))
+{
+    Console.WriteLine("Fizz Buzz");
+}
+else if(a%5==0 || a%3==0 || (a%3==0&&a%5==0))
+{
+    Console.WriteLine("not FizzBuzz");
+}
