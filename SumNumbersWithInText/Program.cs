aad1af5878024407fc8b69bc4dd5622c5648db0b
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-05-27 10:13:13 +0500

    Added new project. Proj name's sum numbers within text

---
+++ SumNumbersWithInText/Program.cs
@@ -0,0 +1,14 @@
+﻿        Console.WriteLine("Matn kiriting:");
+        string matn = Console.ReadLine();
+
+        int yigindi = 0;
+        foreach (char belgi in matn)
+        {
+            if (Char.IsDigit(belgi))
+            {
+                int raqam = int.Parse(belgi.ToString());
+                yigindi += raqam;
+            }
+        }
+
+        Console.WriteLine("Raqamlar yig'indisi: " + yigindi);
