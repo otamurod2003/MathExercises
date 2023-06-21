feef12bd165ef25b2dfc383dababb6df2e471733
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-06-21 17:44:13 +0500

    added new project...

---
+++ SumOfArrayElements/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SumOfArrayElements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nechta raqam kiritasiz:  \n");
+            int number = int.Parse(Console.ReadLine());
+            var numbers = new int[number];
+            
+            for (int num = 0; num < numbers.Length; num++)
+            {
+                Console.Write($"Number {num + 1}:");
+                numbers[num] = int.Parse(Console.ReadLine());
+                
+            }
+
+           int result =  getSum(numbers);
+            Console.WriteLine("Natija: " + result);
+        }
+
+        static int getSum(int[] numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
